c968b74b677af655ba4e57e7601ae7e90c431d69
Anton Dvoininov <antonydvoininow@outlook.com>
2025-02-21 03:37:21 +0300

    fix 2

--- src/layout/components/TicketList/TicketList.tsx
+++ src/layout/components/TicketList/TicketList.tsx
@@ -6,6 +6,8 @@ import { TicketCard } from "../TicketCard/TicketCard";
 import { Button } from "../../ui/Button/Button";
 import { TicketObject } from "../../../api/types/RequestTypes";
 import { MoonLoader } from "react-spinners";
+import { useAppSelector } from "../../../store/hooks";
+import { LoadingStatus } from "../../../store/slices/loading.slice";
 
 export type TicketListProps = {
   tickets: TicketObject[];
@@ -13,6 +15,7 @@ export type TicketListProps = {
 
 export const TicketList: FC<TicketListProps> = ({ tickets }) => {
   const [limit, setLimit] = useState(5);
+  const loading = useAppSelector(state => state.loading.status);
 
   return (
     <main className={classes["ticket-list"]}>
@@ -20,7 +23,12 @@ export const TicketList: FC<TicketListProps> = ({ tickets }) => {
       {tickets.slice(0, limit).map((ticket, index) => (
         <TicketCard key={index} ticket={ticket} />
       ))}
-      {tickets.length === 0 && <div className={classes["ticket-list__spinner"]}><MoonLoader  color="#2196f3" size={30}/> </div>}
+      {loading === LoadingStatus.No && (
+        <div className={classes["ticket-list__spinner"]}>
+          <MoonLoader color="#2196f3" size={30} />
+        </div>
+      )}
+      {tickets.length === 0 && <div className={classes["ticket-list__empty"]}>Empty</div>}
       {tickets.length !== 0 && <Button onClick={() => setLimit(prev => prev + 5)} />}
     </main>
   );
